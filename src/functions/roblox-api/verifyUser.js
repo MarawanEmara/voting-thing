dd398702d9ecfc8b7120628ba9eca88d5c8d862a
Marawan Emara <93253811+MarawanEmara@users.noreply.github.com>
2022-10-06 15:33:18 +0200

    Sentence generation for verification

--- src/functions/roblox-api/verifyUser.js
+++ src/functions/roblox-api/verifyUser.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const noblox = require("noblox.js");
 const { EmbedBuilder } = require("discord.js");
 const verifiedUsers = require("../../schemas/verified-users");
+const Sentencer = require("sentencer");
 
 module.exports = (client) => {
   client.checkUser = async (interaction, userID) => {
@@ -34,15 +35,81 @@ module.exports = (client) => {
                 noblox
                   .getIdFromUsername(username)
                   .then(async (robloxID) => {
-                    newUser = await new verifiedUsers({
-                      _id: mongoose.Types.ObjectId(),
-                      discordID: userID,
-                      robloxID: robloxID,
-                    });
-                    await newUser.save().catch((err) => console.log(err));
-                    interaction.followUp({
-                      embeds: [acceptEmbed("You have been verified.")],
-                    });
+                    try {
+                      pass = await generateSentence(
+                        3 + Math.floor(Math.random() * 10)
+                      );
+                      // Prompt the user with an embed that says to change their description to the pass
+                      interaction
+                        .followUp({
+                          embeds: [
+                            utilsEmbed(
+                              `Please change your Roblox description to \`${pass}\`.`
+                            ),
+                          ],
+                        })
+                        .then(() => {
+                          // Wait 300 seconds for the user to change their description or until they cancel or respond with done
+                          interaction.channel
+                            .awaitMessages(
+                              {
+                                filter,
+                                max: 1,
+                                time: 300000,
+                                errors: ["time"],
+                              },
+                              {
+                                prompt:
+                                  "Please change your Roblox description to the pass.",
+                              }
+                            )
+                            .then((message) => {
+                              if (message.first().content === "done") {
+                                noblox.getBlurb(robloxID).then((blurb) => {
+                                  if (blurb === pass) {
+                                    const verifiedUser = new verifiedUsers({
+                                      _id: mongoose.Types.ObjectId(),
+                                      discordID: userID,
+                                      robloxID: robloxID,
+                                    });
+                                    verifiedUser
+                                      .save()
+                                      .then((result) => {
+                                        console.log(result);
+                                      })
+                                      .catch((err) => console.error(err));
+                                    interaction.followUp({
+                                      embeds: [
+                                        acceptEmbed(
+                                          "You have been verified. Welcome to the server!"
+                                        ),
+                                      ],
+                                    });
+                                  } else {
+                                    interaction.followUp({
+                                      embeds: [
+                                        denyEmbed(
+                                          "You did not change your description to the pass."
+                                        ),
+                                      ],
+                                    });
+                                  }
+                                });
+                              } else {
+                                interaction.followUp({
+                                  embeds: [
+                                    denyEmbed(
+                                      "You did not change your description to the pass."
+                                    ),
+                                  ],
+                                });
+                              }
+                            });
+                        })
+                        .catch((err) => {
+                          console.log(err);
+                        });
+                    } catch (error) {}
                   })
                   .catch((err) => {
                     console.log(err);
@@ -107,6 +174,45 @@ module.exports = (client) => {
           iconURL:
             "https://media-exp1.licdn.com/dms/image/C560BAQEMpZZewXBuJg/company-logo_200_200/0/1531742819850?e=2147483647&v=beta&t=Njb7Jy5UNynPABBaceLL6x3ymTY3civGml0wJdPclhk",
         });
+    }
+
+    function utilsEmbed(message) {
+      return new EmbedBuilder()
+        .setColor("#3498DB")
+        .setTitle("Verification")
+        .setAuthor({
+          name: "Electoral Commission",
+          iconURL:
+            "https://media-exp1.licdn.com/dms/image/C560BAQEMpZZewXBuJg/company-logo_200_200/0/1531742819850?e=2147483647&v=beta&t=Njb7Jy5UNynPABBaceLL6x3ymTY3civGml0wJdPclhk",
+          url: "https://discord.gg/DDdvEbR",
+        })
+        .setDescription(message)
+        .setTimestamp(Date.now())
+        .setFooter({
+          text: "Electoral Commission",
+          iconURL:
+            "https://media-exp1.licdn.com/dms/image/C560BAQEMpZZewXBuJg/company-logo_200_200/0/1531742819850?e=2147483647&v=beta&t=Njb7Jy5UNynPABBaceLL6x3ymTY3civGml0wJdPclhk",
+        });
+    }
+
+    function generateSentence(length) {
+      possibilities = [
+        "{{ an_adjective }}",
+        "{{ a_noun }}",
+        "{{ nouns }}",
+        "{{ noun }}",
+        "{{ adjective }}",
+      ];
+      sentence = "";
+      if (length < 1) {
+        return sentence;
+      }
+      for (i = 0; i < length; i++) {
+        sentence +=
+          possibilities[Math.floor(Math.random() * possibilities.length)];
+      }
+      sentence = Sentencer.make(sentence);
+      return sentence;
     }
   };
 };

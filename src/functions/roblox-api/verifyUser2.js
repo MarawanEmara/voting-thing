eebe958f5e0c9b87becdaa63996472fd50513bcc
Marawan Emara <93253811+MarawanEmara@users.noreply.github.com>
2022-10-09 00:45:02 +0200

    Worked out the try-catch and chaining for promises

--- src/functions/roblox-api/verifyUser2.js
+++ src/functions/roblox-api/verifyUser2.js
@@ -8,7 +8,7 @@ const Sentencer = require("sentencer");
 module.exports = (client) => {
   client.checkUser2 = async (interaction, userID) => {
     const bot = await noblox.setCookie(process.env.ROBLOSECURITY_TOKEN);
-    verifiedUsers.countDocuments({ discordID: userID }, (err, count) => {
+    verifiedUsers.countDocuments({ discordID: userID }, async (err, count) => {
       if (count > 0) {
         const embed = new EmbedBuilder()
           .setTitle("User Verification")
@@ -16,6 +16,7 @@ module.exports = (client) => {
             "You are already verified. If you think this is a mistake, please contact the Electoral Commission."
           )
           .setColor("#57F287");
+
         interaction.user
           .createDM()
           .then((dm) => {
@@ -29,30 +30,216 @@ module.exports = (client) => {
             });
           });
       } else {
+        const filter = (m) => m.author.id === interaction.user.id;
+
+        try {
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription("What is your current Roblox username?")
+            .setColor("#57F287");
+          dm = await interaction.user.createDM();
+          await dm.send({ embeds: [embed] });
+        } catch (error) {
+          await interaction.reply({
+            content:
+              "I could not DM you. Please enable DMs from server members.",
+            ephemeral: true,
+          });
+          return;
+        }
+
+        let username;
+
+        try {
+          username = await getUsername(dm, filter);
+        } catch (error) {
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription(
+              "You did not respond in time. Please try again later."
+            )
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+          return;
+        }
+
+        let robloxID;
+
+        try {
+          robloxID = await noblox.getIdFromUsername(username);
+        } catch (error) {
+          console.log(error);
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription("That username does not exist. Please try again.")
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+          username = await getUsername(dm, filter);
+          try {
+            robloxID = await noblox.getIdFromUsername(username);
+          } catch (error) {
+            const embed = new EmbedBuilder()
+              .setTitle("User Verification")
+              .setDescription(
+                "That username does not exist. Please try again later."
+              )
+              .setColor("#57F287");
+            dm.send({ embeds: [embed] });
+            return;
+          }
+        }
+
+        const verificationString = generateSentence(
+          Math.floor(Math.random() * 5) + 3
+        );
+        try {
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription(
+              `Please include the following in your Roblox description: ${verificationString}`
+            )
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+        } catch (error) {
+          await interaction.reply({
+            content:
+              "I could not DM you. Please enable DMs from server members.",
+            ephemeral: true,
+          });
+          return;
+        }
+
+        let awaitVerification;
+
+        try {
+          awaitVerification = await beginVerification(interaction, dm);
+        } catch (error) {
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription(
+              "You did not respond in time. Please try again later."
+            )
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+          return;
+        }
+
+        if (awaitVerification.first().content !== verificationString) {
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription(
+              "You did not include the correct verification string. Please try again."
+            )
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+          try {
+            awaitVerification = await beginVerification(interaction, dm);
+          } catch (error) {
+            const embed = new EmbedBuilder()
+              .setTitle("User Verification")
+              .setDescription(
+                "You did not respond in time. Please try again later."
+              )
+              .setColor("#57F287");
+            dm.send({ embeds: [embed] });
+            return;
+          }
+        }
+
+        if (awaitVerification.first().content !== verificationString) {
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription(
+              "You did not include the correct verification string. Please try again later."
+            )
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+          return;
+        }
+
+        // Add user to database
+        const newUser = new verifiedUsers({
+          _id: mongoose.Types.ObjectId(),
+          discordID: userID,
+          robloxID: robloxID,
+          robloxUsername: username,
+        });
+
+        newUser.save().catch((err) => {
+          console.log(err);
+          const embed = new EmbedBuilder()
+            .setTitle("User Verification")
+            .setDescription(
+              "There was an error saving your data. Please try again later."
+            )
+            .setColor("#57F287");
+          dm.send({ embeds: [embed] });
+          return;
+        });
+
         const embed = new EmbedBuilder()
           .setTitle("User Verification")
-          .setDescription("What is your current Roblox username?")
+          .setDescription("You have been successfully verified.")
           .setColor("#57F287");
-        interaction.user
-          .createDM()
-          .then((dm) => {
-            dm.send({ embeds: [embed] });
-            const filter = (m) => m.author.id === interaction.user.id;
-            dm.awaitMessages({ filter, max: 1, time: 60000, errors: ["time"] })
-              .then((message) => {
-                const username = message.first().content;
-                try {
-                } catch (err) {}
-              })
-              .catch((err) => {});
-          })
-          .catch((err) => {
-            interaction.reply({
-              content:
-                "You are not verified. I have attempted to DM you that, but could not.",
-              ephemeral: true,
-            });
+        dm.send({ embeds: [embed] }).catch((err) => {
+          interaction.reply({
+            content:
+              "You have been successfully verified. I have attempted to DM you that, but could not.",
+            ephemeral: true,
           });
+        });
+      }
+
+      async function getUsername(dm, filter) {
+        try {
+          const awaitUsername = await dm.awaitMessages({
+            filter,
+            max: 1,
+            time: 60000,
+            errors: ["time"],
+          });
+          return awaitUsername.first().content;
+        } catch (error) {
+          throw new Error("User did not respond in time.");
+        }
+      }
+
+      async function beginVerification(interaction, dm) {
+        try {
+          filter = (m) =>
+            m.author.id === interaction.user.id &&
+            m.content.toLowerCase().includes("done");
+          const awaitVerification = await dm.awaitMessages({
+            filter,
+            max: 1,
+            time: 60000,
+            errors: ["time"],
+          });
+          return awaitVerification;
+        } catch (error) {
+          console.log(error);
+          throw new Error("User did not respond in time.");
+        }
+      }
+
+      function generateSentence(length) {
+        possibilities = [
+          "{{ an_adjective }}",
+          "{{ a_noun }}",
+          "{{ nouns }}",
+          "{{ noun }}",
+          "{{ adjective }}",
+        ];
+        sentence = "";
+        if (length < 1) {
+          return sentence;
+        }
+        for (i = 0; i < length; i++) {
+          sentence +=
+            possibilities[Math.floor(Math.random() * possibilities.length)];
+        }
+        sentence = Sentencer.make(sentence);
+        return "dncmark";
       }
     });
   };
---
+++ src/schemas/election.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const partySchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  partyName: String,
+  partyLeader: String,
+  partyManifesto: String,
+});
+
+module.exports = new model("Party", partySchema, "parties");
+
+const electionSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  electionName: String,
+  creatorID: String,
+  groupID: String,
+  parties: [[partySchema]],
+  time: { type: Number, min: 12, max: 48, default: 24 },
+  date: { type: Date, default: Date.now },
+  lastUpdated: { type: Date, default: Date.now },
+});
+
+module.exports = new model("Election", electionSchema, "elections");
